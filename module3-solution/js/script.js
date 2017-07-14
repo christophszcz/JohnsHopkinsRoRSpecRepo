be2eee6e7faee2b3a21a0099f862b6945dfc8003
christophszcz <christoph.szczechowicz@gmail.com>
2017-07-14 16:09:02 -0400

    Make the menu button white upon clicking it.

---
+++ module3-solution/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$('button').click(function(){
+		$('.navbar-default .navbar-toggle').toggleClass('white-border');
+		$('.navbar-default .navbar-toggle .icon-bar').toggleClass('white-bar');
+	});
+});
