34812f65459eadced5ddaa9de3db6854143ab0a9
christophszcz <christoph.szczechowicz@gmail.com>
2017-07-15 22:16:26 -0400

    Make the navbar border thinner during mobile mode.

--- module3-solution/js/script.js
+++ module3-solution/js/script.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
 	$('button').click(function(){
 		$('.navbar-default .navbar-toggle').toggleClass('white-border');
 		$('.navbar-default .navbar-toggle .icon-bar').toggleClass('white-bar');
+		$('#navbar-stretch').toggleClass('navbar-thinner');
 	});
 });
